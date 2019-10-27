727f4246438789b2bc088644b0664327c4d3cd4f
JoshuaShepard87 <jburden@mail.mccneb.edu>
2019-10-26 23:03:27 -0500

    a java file!

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main{
+    public static void main(String[] args){
+        System.out.println("Hello Please!");
+        int a = 5;
+        int b = 3;
+
+        System.out.println("the number of int a is: " +a + "\n the number of b is " + b+ "\n the number timed each other is " + (a*b));
+    }
+}
